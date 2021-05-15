7e957d87d4fb4c367d700cc39582c9bb362dff64
Pramod Gupta <pga286478@gmail.com>
2021-05-16 02:11:05 +0530

    Landing page changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 // eslint-disable-next-line
 import logo from './logo.svg';
 import chart from './undraw_Data_points_re_vkpq.svg'
+import graphic1 from './undraw_data_processing_yrrv.svg'
 import './App.css';
 const FileDownload = require('js-file-download');
 
@@ -20,7 +21,7 @@ function App() {
 	const handleSubmission = () => {
 		const formData = new FormData();
 
-    setMessage("Your report is being generated, this will take a couple of seconds. Please Wait....");
+    setMessage("Your report is being generated, Please Wait....");
 
     formData.append('file', selectedFile);
 
@@ -76,36 +77,44 @@ function App() {
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         {/* <p> Current Time is {currentTime} </p> */}
         
-        <div id="large-font">
+        <div id="Description-text">
           {/* <p>WORLDS BEST ANALYTICS TOOL</p> */}
           <h3> Understand Your Data <br/> Like Never Before </h3>
-
+          <p className="Description">
+            Insight-Generator is world's first patform to <br/> get Insights of any csv file withim seconds.   
+          </p>
         </div>
-        
-        <div>
+
+
+        <div className="Converter-container">
           <p> Attach Your CSV File Here ... </p>
-          <input type="file" name="file" onChange={changeHandler} />
+          <input type="file" name="file" onChange={changeHandler} className="" />
           {isSelected ? (
             <div>
-              <p>Filename: {selectedFile.name}</p>
+              {/* <p>Filename: {selectedFile.name}</p>
               <p>Filetype: {selectedFile.type}</p>
               <p>Size in bytes: {selectedFile.size}</p>
               <p>
                 lastModifiedDate:{' '}
                 {selectedFile.lastModifiedDate.toLocaleDateString()}
-              </p>
+              </p> */}
             </div>
           ) : (
             // <p>Select a file to show details</p>
             <p></p>
           )}
-          <div>
-            <button onClick={handleSubmission}>Get Report</button>
+          <div className="Div-button" onClick={handleSubmission}>
+            <div className="Button-center" >Get Report</div>
           </div>
-          <div> 
-            <p>{message}</p>
-          </div>
+          {/* <div> 
+            <div>{message}</div>
+          </div> */}
       </div>
+      {/* <br/>
+      <br/>
+      <div>
+          <img src={graphic1} className="Graphic1" alt="logo" />
+      </div> */}
 
       </div>
 
