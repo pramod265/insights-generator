7b821b08748dbfcbc0901d9d8f233e54c996c43b
Pramod Gupta <pga286478@gmail.com>
2021-05-15 05:34:54 +0530

    lading page code updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 // eslint-disable-next-line
 import logo from './logo.svg';
+import chart from './undraw_Data_points_re_vkpq.svg'
 import './App.css';
 const FileDownload = require('js-file-download');
 
 function App() {
 
   const [currentTime, setCurrentTime] = useState(0);
+  const [message, setMessage] = useState("");
 	const [selectedFile, setSelectedFile] = useState();
 	const [isSelected, setIsSelected] = useState(false);
 
@@ -18,7 +20,9 @@ function App() {
 	const handleSubmission = () => {
 		const formData = new FormData();
 
-		formData.append('file', selectedFile);
+    setMessage("Your report is being generated, this will take a couple of seconds. Please Wait....");
+
+    formData.append('file', selectedFile);
 
 		fetch(
 			'http://127.0.0.1:5000/generate_report',
@@ -36,6 +40,7 @@ function App() {
 			.catch((error) => {
 				console.error('Error:', error);
 			});
+    // setMessage("Done Thanks for using Insight Generator");
 	};
 	
 
@@ -52,11 +57,33 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+    <div className="Menu-bar">
+    <div id="home-name" > Insight-Generator </div>
+      <nav id="main-menu">
+          <ul class="nav-bar">
+                <li class="nav-button-home"><a href="#">Home</a></li>
+                <li class="nav-button-services"><a href="#">Services</a></li>
+                <li class="nav-button-products"><a href="#">About</a></li>
+          </ul>
+      </nav>    
+    </div>
+    
+    <div className="Chart-container">
+      <img src={chart} className="Chart" alt="logo" />
+    </div>
+   
+    <div className="Main-content">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p> Current Time is {currentTime} </p>
+        {/* <p> Current Time is {currentTime} </p> */}
+        
+        <div id="large-font">
+          {/* <p>WORLDS BEST ANALYTICS TOOL</p> */}
+          <h3> Understand Your Data <br/> Like Never Before </h3>
 
+        </div>
+        
         <div>
+          <p> Attach Your CSV File Here ... </p>
           <input type="file" name="file" onChange={changeHandler} />
           {isSelected ? (
             <div>
@@ -69,16 +96,18 @@ function App() {
               </p>
             </div>
           ) : (
-            <p>Select a file to show details</p>
+            // <p>Select a file to show details</p>
+            <p></p>
           )}
           <div>
-            <button onClick={handleSubmission}>Submit</button>
+            <button onClick={handleSubmission}>Get Report</button>
+          </div>
+          <div> 
+            <p>{message}</p>
           </div>
       </div>
 
-
-
-      </header>
+      </div>
 
     </div>
   );
